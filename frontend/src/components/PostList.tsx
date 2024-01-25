6cb32dbda0650067abfdc572bd9890594fa95413
hugoradiawan <43243582+hugoradiawan@users.noreply.github.com>
2024-01-25 02:51:40 +0000

    post update

--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Modal } from "./Modal";
 import { NewPost } from "./NewPost";
 import { Post } from "./Post";
@@ -10,9 +10,29 @@ export function PostList({
 }: Readonly<{ isPosting: boolean; onStopPosting: () => void }>): JSX.Element {
   let modalContent: JSX.Element | null = null;
   const [posts, setPosts] = useState<Post[]>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   function addPosthandler(postData: Post): void {
+    fetch('https://expert-space-winner-rp4g7gxg76vfxw5q-8080.app.github.dev/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
     setPosts(prevPosts => [postData, ...prevPosts]);
+  }
+
+  async function fetchPosts(): Promise<void> {
+    setIsFetching(true);
+    const res: Response = await fetch('https://expert-space-winner-rp4g7gxg76vfxw5q-8080.app.github.dev/posts');
+    const data: PostResponse = await res.json();
+    setPosts(data.posts);
+    setIsFetching(false);
   }
 
   if (isPosting) {
@@ -26,7 +46,7 @@ export function PostList({
   return (
     <>
       {modalContent}
-      {posts.length > 0 ? (
+      {!isFetching && posts.length > 0 ? (
         (
           <ul className={classes.posts}>
             {posts.map((post) => (
@@ -35,11 +55,16 @@ export function PostList({
           </ul>
         )
       ) : null}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
       <div style={{textAlign: 'center', color: 'white'}}>
         <h2>There are no posts yet.</h2>
         <p>Start adding some!</p>
       </div>
+      )}
+      {isFetching && (
+        <div style={{textAlign: 'center', color: 'white'}}>
+          <h2>Loading...</h2>
+        </div>
       )}
     </>
   );
--- frontend/src/components/defs.d.ts
+++ frontend/src/components/defs.d.ts
@@ -1,4 +1,9 @@
 interface Post {
+    id?: string;
     body: string;
     author: string;
+}
+
+interface PostResponse {
+    posts: Post[];
 }
